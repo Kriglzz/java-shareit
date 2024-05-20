6f558d50e3e903313b26a96498a970e9095af1c9
Kriglz <Kriglz@yandex.ru>
2024-05-20 22:43:35 +0300

    IllegalStateException (Не помогло)

--- src/main/java/ru/practicum/shareit/exception/IllegalStateException.java
+++ src/main/java/ru/practicum/shareit/exception/IllegalStateException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class IllegalStateException extends RuntimeException{
+public class IllegalStateException extends RuntimeException {
     public IllegalStateException(String message) {
         super(message);
     }
