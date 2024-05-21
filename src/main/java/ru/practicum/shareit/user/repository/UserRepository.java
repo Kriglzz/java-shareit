b7d2dfc8f23b867651a68b88003bdfe6bec5b66b
Kriglz <Kriglz@yandex.ru>
2024-05-21 21:52:17 +0300

    Is it working?

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
 
@@ -10,9 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
-
-    @Query(value = "SELECT COALESCE(MAX(user_id), 0) FROM users", nativeQuery = true)
-    Long findMaxId();
 
     Optional<User> findByEmail(String email);
 }
