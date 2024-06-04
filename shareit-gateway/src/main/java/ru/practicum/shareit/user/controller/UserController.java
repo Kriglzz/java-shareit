65e1be49672ecb7736ca4233f785b80980737f61
Kriglz <Kriglz@yandex.ru>
2024-06-04 23:40:09 +0300

    I DONT UNDERSTAND ENYTHING

---
+++ shareit-gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.UserClient;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.Valid;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@RestController
+@RequestMapping("/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+    private final UserClient userClient;
+
+    @PostMapping
+    public ResponseEntity<Object>  addUser(@Valid @RequestBody UserDto userDto) {
+        log.info("Post user with userDto={}", userDto);
+        return userClient.postUser(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody UserDto user) {
+        return userClient.updateUser(userId, user);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        log.info("Get users");
+        return userClient.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUserById(@PathVariable Long userId) {
+        return userClient.getUserById(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUserById(@PathVariable Long userId) {
+        return userClient.deleteUserById(userId);
+    }
+}
