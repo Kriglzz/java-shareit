2222e44ace2e7039a982e8da4f282cd843d71829
Kriglz <Kriglz@yandex.ru>
2024-06-06 20:12:30 +0300

    what

--- shareit-gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ shareit-gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -18,6 +18,7 @@ public class UserController {
         return userClient.postUser(userDto);
     }
 
+
     @PatchMapping("/{userId}")
     public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody UserDto user) {
         return userClient.updateUser(userId, user);
