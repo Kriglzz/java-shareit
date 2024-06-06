e583f3aad893c4847bd3557e9f86661143b9eaac
Kriglz <Kriglz@yandex.ru>
2024-06-06 20:03:52 +0300

    final?

--- shareit-server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ shareit-server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -3,13 +3,7 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-
 @SpringBootApplication
-@PropertySource(value = "classpath:application.properties")
-@PropertySource(value = "classpath:runtime.properties", ignoreResourceNotFound = true)
 public class ShareItServer {
 
     public static void main(String[] args) {
