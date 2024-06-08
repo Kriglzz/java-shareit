7409ff6db7f596571b5c3047fe9845d591ea6921
Kriglz <Kriglz@yandex.ru>
2024-06-08 16:22:18 +0300

    root problem

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server {
+public class ShareItServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Server.class, args);
+        SpringApplication.run(ShareItServer.class, args);
     }
 
 }
