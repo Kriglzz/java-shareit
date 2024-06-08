73349f0e0557ceb661f5ea670fe902c36bef1a78
Kriglz <Kriglz@yandex.ru>
2024-06-08 14:40:16 +0300

    sad thing

--- gateway/src/main/java/ru/practicum/shareit/Gateway.java
+++ gateway/src/main/java/ru/practicum/shareit/Gateway.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer {
-
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(ShareItServer.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 
 }
